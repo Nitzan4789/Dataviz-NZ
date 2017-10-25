ada13f08d6d596f916b2aa4b8d1caa5f0466d148
Nitzan4789 <32143034+Nitzan4789@users.noreply.github.com>
2017-10-25 18:16:21 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,55 +1,178 @@
-/*
-//map using d3+ (color the countries)
-var sample_data = [
-    {"year": "1992 - 2014", "country": "aschn", "name": "China", "tags": "birth country; undergraduate", "color": "#ABDDA4"},
-    {"year": "2014", "country": "eudeu", "name": "Germany", "tags": "conference", "color": "#ABDDA4"},
-    {"year": "2014 - 2017", "country": "nausa", "name": "United States", "tags": "graduate", "color": "#ABDDA4"},
-    {"year": "2016", "country": "nabhs", "name": "Bahamas", "tags": "travel", "color": "#ABDDA4"},
-    {"year": "2017", "country": "sachl", "name": "Chile", "tags": "research", "color": "#ABDDA4"}
-];
+<!DOCTYPE html>
+<meta charset="utf-8">
+<style>
 
-var visualization = d3plus.viz()
-    .container("#svg_map")        // container DIV to hold the visualization
-    .data(sample_data)            // data to use with the visualization
-    .coords({
-        "mute": ["anata"],
-        "value": "http://d3plus.org/topojson/countries.json"
-    }) // pass topojson coordinates
-    .type("geo_map")              // visualization type
-    .id("country")                // key for which our data is unique on
-    .text("name")                 // key to use for display text
-    .color("color")               // key for coloring countries
-    .tooltip(["year", "tags"])              // keys to place in tooltip
-    .draw();                      // finally, draw the visualization!
-*/
+svg {
+  font: 10px sans-serif;
+}
 
-//using datamaps (d3) http://datamaps.github.io
-var basic_choropleth = new Datamap({
-  element: document.getElementById("svg_map"),
-  projection: 'mercator',
-  fills: {
-    defaultFill: "#CCCCCC",
-    authorHasTraveledTo: "#ABDDA4",
-    authorHasStudiedIn: "#AB55A4"
-  },
-  data: {
-    CHN: { fillKey: "authorHasStudiedIn", year: "1992 - 2014" },
-    DEU: { fillKey: "authorHasTraveledTo", year: "2014" },
-    USA: { fillKey: "authorHasStudiedIn", year: "2014 - 2017" },
-    BHS: { fillKey: "authorHasTraveledTo", year: "2016" },
-    CHL: { fillKey: "authorHasTraveledTo", year: "2017" }
-  },
-  geographyConfig: {
-    popupTemplate: function(geo, data) {
-        return ['<div class="hoverinfo"><strong>',
-                'Country: ' + geo.properties.name,
-                '<br> Year: ' + data.year,
-                '</strong></div>'].join('');
+.y.axis path {
+  display: none;
+}
+
+.y.axis line {
+  stroke: #fff;
+  stroke-opacity: .2;
+  shape-rendering: crispEdges;
+}
+
+.y.axis .zero line {
+  stroke: #000;
+  stroke-opacity: 1;
+}
+
+.title {
+  font: 300 78px Helvetica Neue;
+  fill: #666;
+}
+
+.birthyear,
+.age {
+  text-anchor: middle;
+}
+
+.birthyear {
+  fill: #fff;
+}
+
+rect {
+  fill-opacity: .6;
+  fill: #e377c2;
+}
+
+rect:first-child {
+  fill: #1f77b4;
+}
+
+</style>
+<body>
+<script src="//d3js.org/d3.v3.min.js"></script>
+<script>
+
+var margin = {top: 20, right: 40, bottom: 30, left: 20},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    barWidth = Math.floor(width / 19) - 1;
+
+var x = d3.scale.linear()
+    .range([barWidth / 2, width - barWidth / 2]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("right")
+    .tickSize(-width)
+    .tickFormat(function(d) { return Math.round(d / 1e6) + "M"; });
+
+// An SVG element with a bottom-right origin.
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// A sliding container to hold the bars by birthyear.
+var birthyears = svg.append("g")
+    .attr("class", "birthyears");
+
+// A label for the current year.
+var title = svg.append("text")
+    .attr("class", "title")
+    .attr("dy", ".71em")
+    .text(2000);
+
+d3.csv("population.csv", function(error, data) {
+
+  // Convert strings to numbers.
+  data.forEach(function(d) {
+    d.people = +d.people;
+    d.year = +d.year;
+    d.age = +d.age;
+  });
+
+  // Compute the extent of the data set in age and years.
+  var age1 = d3.max(data, function(d) { return d.age; }),
+      year0 = d3.min(data, function(d) { return d.year; }),
+      year1 = d3.max(data, function(d) { return d.year; }),
+      year = year1;
+
+  // Update the scale domains.
+  x.domain([year1 - age1, year1]);
+  y.domain([0, d3.max(data, function(d) { return d.people; })]);
+
+  // Produce a map from year and birthyear to [male, female].
+  data = d3.nest()
+      .key(function(d) { return d.year; })
+      .key(function(d) { return d.year - d.age; })
+      .rollup(function(v) { return v.map(function(d) { return d.people; }); })
+      .map(data);
+
+  // Add an axis to show the population values.
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + width + ",0)")
+      .call(yAxis)
+    .selectAll("g")
+    .filter(function(value) { return !value; })
+      .classed("zero", true);
+
+  // Add labeled rects for each birthyear (so that no enter or exit is required).
+  var birthyear = birthyears.selectAll(".birthyear")
+      .data(d3.range(year0 - age1, year1 + 1, 5))
+    .enter().append("g")
+      .attr("class", "birthyear")
+      .attr("transform", function(birthyear) { return "translate(" + x(birthyear) + ",0)"; });
+
+  birthyear.selectAll("rect")
+      .data(function(birthyear) { return data[year][birthyear] || [0, 0]; })
+    .enter().append("rect")
+      .attr("x", -barWidth / 2)
+      .attr("width", barWidth)
+      .attr("y", y)
+      .attr("height", function(value) { return height - y(value); });
+
+  // Add labels to show birthyear.
+  birthyear.append("text")
+      .attr("y", height - 4)
+      .text(function(birthyear) { return birthyear; });
+
+  // Add labels to show age (separate; not animated).
+  svg.selectAll(".age")
+      .data(d3.range(0, age1 + 1, 5))
+    .enter().append("text")
+      .attr("class", "age")
+      .attr("x", function(age) { return x(year - age); })
+      .attr("y", height + 4)
+      .attr("dy", ".71em")
+      .text(function(age) { return age; });
+
+  // Allow the arrow keys to change the displayed year.
+  window.focus();
+  d3.select(window).on("keydown", function() {
+    switch (d3.event.keyCode) {
+      case 37: year = Math.max(year0, year - 10); break;
+      case 39: year = Math.min(year1, year + 10); break;
     }
+    update();
+  });
+
+  function update() {
+    if (!(year in data)) return;
+    title.text(year);
+
+    birthyears.transition()
+        .duration(750)
+        .attr("transform", "translate(" + (x(year1) - x(year)) + ",0)");
+
+    birthyear.selectAll("rect")
+        .data(function(birthyear) { return data[year][birthyear] || [0, 0]; })
+      .transition()
+        .duration(750)
+        .attr("y", y)
+        .attr("height", function(value) { return height - y(value); });
   }
 });
-var legend = d3.select("#svg_map").append("svg").attr("id", "legend");
-legend.append("rect").attr("x", "200").attr("y", "10").attr("width", "20").attr("height", "20").attr("fill", "#ABDDA4");
-legend.append("text").attr("x", "225").attr("y", "24").attr("font-size", "12px").text("Traveled to");
-legend.append("rect").attr("x", "350").attr("y", "10").attr("width", "20").attr("height", "20").attr("fill", "#AB55A4");
-legend.append("text").attr("x", "375").attr("y", "24").attr("font-size", "12px").text("Studied in");
+
+</script>
